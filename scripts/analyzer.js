c7fe65487deac387d0e80a0c31281e7176da89cc
Vladislav Manakov <footzi@mail.ru>
2018-10-14 18:59:12 +0300

    add animation open video

--- scripts/analyzer.js
+++ scripts/analyzer.js
@@ -1,21 +1,25 @@
 class Analyzer {
-    constructor () {
-        this.canvas = document.querySelector('.b-popup__analyzer');
-        this.warning = document.querySelector('.b-popup__no-signal');
+    constructor() {
+        this.wrapper = document.querySelector('.j-analyzer');
+        this.canvas = this.wrapper.querySelector('canvas');
+        this.warning = this.wrapper.querySelector('.j-analyzer-no-signal');
         this.ctx = this.canvas.getContext('2d');
         this.result = [];
         this.counterBars = 8;
         this.bar = {};
         this._addGradient();
+        this.mediaElement = new WeakMap();
+
+        this.counter = 0;
     }
 
     /**
      * Метод создает градиент
      */
     _addGradient() {
-        this.gragient = this.ctx.createLinearGradient(0,0,0,300);
-        this.gragient.addColorStop(1,'#ffe682');
-        this.gragient.addColorStop(0,'#ff9e00');
+        this.gragient = this.ctx.createLinearGradient(0, 0, 0, 300);
+        this.gragient.addColorStop(1, "#ffe682");
+        this.gragient.addColorStop(0, "#ff9e00");
     }
 
     /**
@@ -26,20 +30,37 @@ class Analyzer {
         const AudioContext = window.AudioContext || window.webkitAudioContext;
 
         if (AudioContext) {
-            const context = new AudioContext();
 
-            this.analyser = context.createAnalyser();
+            if (!this.context) {
+                this.context =  new AudioContext();
+            }
+
+            this.analyser = this.context.createAnalyser();
             this.analyser.fftSize = 256;
-            const source = context.createMediaElementSource(video);
-
-            source.connect(this.analyser);
-            this.analyser.connect(context.destination)
-
-            this.bufferLength = this.analyser.frequencyBinCount;
-            this.dataArray = new Uint8Array(this.bufferLength);
-
-            this._createBars();
+            
+            if (this.mediaElement.has(video)) {
+                this.source = this.mediaElement.get(video);
+            } else {
+                this.source = this.context.createMediaElementSource(video);
+                this.mediaElement.set(video, this.source);
+                this.source.connect(this.analyser);
+                this.analyser.connect(this.context.destination); 
+                this._createBars();
+            }
         }
+    }
+
+    /**
+     * Метод должен закрывать все существующие коннекты к медиаэлементам
+     */
+    destroy() {;
+        this.context = '';
+
+        // при повторном открытии элемента не происходит дисконект
+        this.source.disconnect();
+        this.analyser.disconnect();
+
+        cancelAnimationFrame(this.myReq);
     }
 
     /**
@@ -47,30 +68,35 @@ class Analyzer {
      */
     _createBars() {
         const loop = () => {
-            requestAnimationFrame(loop);
+            this.myReq = requestAnimationFrame(loop);
 
             this.result = new Uint8Array(this.analyser.frequencyBinCount);
             this.analyser.getByteFrequencyData(this.result);
 
-            this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
             this.ctx.fillStyle = this.gragient;
 
             for (let i = 0; i < this.counterBars; i++) {
-                const position = this.result[i]
-                
+                const position = this.result[i];
+
                 if (position) {
-                    this.warning.style.display = 'none';
+                    this.warning.style.display = "none";
                 } else {
-                    this.warning.style.display = 'block';
+                    this.warning.style.display = "block";
                 }
 
                 this.bar.x = i * 12;
                 this.bar.width = 10;
-                this.bar.height = - position;
-        
-                this.ctx.fillRect(this.bar.x, this.canvas.height, this.bar.width, this.bar.height)
+                this.bar.height = -position;
+
+                this.ctx.fillRect(
+                    this.bar.x,
+                    this.canvas.height,
+                    this.bar.width,
+                    this.bar.height
+                );
             }
-        }
+        };
 
         loop();
     }
--- scripts/cameras.js
+++ scripts/cameras.js
@@ -1,52 +1,138 @@
+import Analyzer from './analyzer';
 import Hls from 'hls.js';
-import Popup from './popup';
+import Filter from './filter';
 
-const popup = new Popup;
-
-const bindEvents = (video) => {
-    const article = video.parentNode;
-    article.addEventListener('click', () => {
-        popup.insertVideo(article, video);
-    })
-}
-
-const videos = [
-    {                                                                                                                                                                                                                                                                                                                                                                                                   
-        target: "#camera-1",
-        url   :"http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fsosed%2Fmaster.m3u8"
-    },{                                                                                                                                                
-        target: "#camera-2",
-        url   :"http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fcat%2Fmaster.m3u8"
-    },{
-        target: "#camera-3",
-        url   : "http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fdog%2Fmaster.m3u8"
-    },{
-        target: "#camera-4",
-        url   : "http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fhall%2Fmaster.m3u8"
+class Camera {
+    constructor() {
+        this.wrapper = document.querySelector('.j-cameras');
+        this.closeButton = document.querySelector('.j-button-close');
+        this.controls = document.querySelector('.j-controls');
+        this.filterWrap = this.controls.querySelector('.j-filters');
+        this.analyzerWrap = this.controls.querySelector('.j-analyzer');
+        this.videos = [];
+        this.analyzer = new Analyzer();
+        this.filter = new Filter();
+        this._bindEvents();
     }
-];
 
-export default function cameras() {
-    for (let item in videos) {
-        const video = document.querySelector(videos[item].target);
-        const url = videos[item].url
+    /**
+     * Метод подключает потоки и вставляет их в соответствующие теги video.
+     * @public
+     */
+    init(videos) {
+        for (let item in videos) {
+            const video = document.querySelector(videos[item].target);
+            const url = videos[item].url;
 
-        if (Hls.isSupported()) {
-            const hls = new Hls();
+            this.videos.push(video);
     
-            hls.attachMedia(video);
+            if (Hls.isSupported()) {
+                const hls = new Hls();
+
+                hls.attachMedia(video);
     
-            hls.on(Hls.Events.MEDIA_ATTACHED, function() {
-                hls.loadSource(url);
-                hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
-                    bindEvents(video);
+                hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+                    hls.loadSource(url);
+                    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                        this._bindEventsVideo(video);
+                        video.play();
+                    });
                 });
-            });
-        } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-            video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
-            video.addEventListener('loadedmetadata',function() {
-                bindEvents(video);
-            });
-          }
+            } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+                video.src ='https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
+                video.addEventListener('loadedmetadata', () => {
+                    this._bindEventsVideo(video);
+                    video.play();
+                });
+            }
+        }
     }
+
+    /**
+     * Навешивает обработчики на требуемые элементы
+     */
+    _bindEvents() {
+        this.closeButton.addEventListener('click', () => {
+            this._close();
+        })
+    }
+
+    /**
+     * Метод навешивает обработчики событий на конкертный элемент камеры
+     * @private
+     */
+    _bindEventsVideo(video) {
+        video.addEventListener('click', (event) => {
+            this.currentVideo = event.currentTarget;
+            this._open();
+        });
+    }
+
+    /**
+     * Метод открывает камеру и инитит модули фильтра и анализатора
+     */
+    _open() {
+        this.currentVideo.setAttribute('controls', 'controls');
+        this.filter.init(this.currentVideo);
+        this.analyzer.init(this.currentVideo);
+
+        this._setScale();
+        this._showControls();
+    }
+
+    /**
+     * Метод закрывает камеру и дестроит модули фильтра и анализатора
+     */
+    _close() {
+        this.currentVideo.removeAttribute('controls');
+        this.currentVideo.style.transform = 'scale(1)';
+        this.filter.destroy();
+        this.analyzer.destroy()
+
+        this._сloseControls();
+    }
+
+    /**
+     * Метод расчитывает масштаб увеличения видео исходя из его ширины
+     */
+    _setScale() {
+        const widthScreen = this.wrapper.offsetWidth;
+        const widthVideo = this.currentVideo.offsetWidth;
+        const scale = widthScreen / widthVideo;
+
+        this.currentVideo.style.transform = `scale(${scale})`;
+    }
+
+    /**
+     * Метод показывает контролы
+     */
+    _showControls() {
+        this.closeButton.classList.add('is-open');
+        this.filterWrap.classList.add('is-open');
+        this.analyzerWrap.classList.add('is-open');
+
+        // Cкрывает все видео кроме активного
+        this.videos.forEach((item) => {
+            if (item != this.currentVideo) {
+                item.style.display = 'none';
+            }
+        })
+    }
+
+    /**
+     * Скрывает контролы
+     */
+    _сloseControls() {
+        this.closeButton.classList.remove('is-open');
+        this.filterWrap.classList.remove('is-open');
+        this.analyzerWrap.classList.remove('is-open');
+
+        // Cкрывает все видео кроме активного
+        this.videos.forEach((item) => {
+            item.style.display = 'block';
+        })
+    }
+
 }
+
+export default Camera;
